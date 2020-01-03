762ab4837800847c01aeae2d20d5f4ba04d0a27e
Alvise Susmel <alvise@poeticoding.com>
2020-01-03 17:36:55 +0100

    added UUID library

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule Yolo.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:uuid, "~> 1.1"},
     ]
   end
 end
